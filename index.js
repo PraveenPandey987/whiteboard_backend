0a34ccf0634a404820a105798ec46d92b21fd23f
praveen <pp7007144435@gmail.com>
2025-07-14 11:09:38 +0530

    deployment_err_try

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
+         transports: ['polling'],
         origin: [
             'https://whiteboard-app-zeta.vercel.app',
             'http://localhost:5173',
